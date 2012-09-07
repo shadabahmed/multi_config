524ddffeeb671f8552bb0105636e413b76fb774a
Shadab Ahmed <shadab_ahmed@mckinsey.com>
2012-09-08 02:53:16 +0530

    First working version

--- lib/multi_config/orms/active_record.rb
+++ lib/multi_config/orms/active_record.rb
@@ -1,38 +1,49 @@
 module MultiConfig
   module ORMs
     module ActiveRecord
-      def config_file=(file_name)
-        namespace = File.basename(file_name, '.yml')
-        @@db_configs ||= []
-        unless @@db_configs.include?(namespace)
-          configurations.merge!(config(file_name,namespace))
-          @@db_configs << namespace
-        end
-        unless configurations.include? "#{namespace}_#{::Rails.env}"
-          raise "#{::Rails.env} config not defined in #{file_name}"
-        end
-        establish_connection "#{namespace}_#{::Rails.env}"
+      def self.included(mod)
+        mod.extend ClassMethods
+        mod.send(:class_variable_set, :'@@db_configs', [])
       end
 
-      private
-
-      def config_path(file_name)
-        file_name = File.join(file_name, '.yml') unless File.extname(file_name).eql? '.yml'
-        File.join(Rails.root, 'config', file_name)
-      end
-
-      def config(file_name, namespace)
-        begin
-          config = {}
-          YAML.load(ERB.new(File.read(config_path(file_name))).result).each do |k,v|
-            config["#{namespace}_#{k}"] = v
+      module ClassMethods
+        def config_file=(file_name)
+          file_name += '.yml' unless File.extname(file_name).eql? '.yml'
+          namespace = File.basename(file_name, '.yml')
+          add_to_db_configs(file_name, namespace)
+          unless configurations.include? "#{namespace}_#{::Rails.env}"
+            raise "Configuration for #{::Rails.env} environment not defined in #{file_name}"
           end
-        rescue
-          raise "File #{file_name} does not exist in config" unless File.exists?(file)
-          raise "Invalid config file #{file_name}"
+          establish_connection "#{namespace}_#{::Rails.env}"
+        end
+
+        private
+
+        def add_to_db_configs(file_name, namespace)
+          db_configs = class_variable_get(:'@@db_configs')
+          unless db_configs.include?(namespace)
+            configurations.merge!(config(file_name, namespace))
+            db_configs << namespace
+          end
+        end
+
+        def config_path(file_name)
+          File.join(Rails.root, 'config', file_name)
+        end
+
+        def config(file_name, namespace)
+          begin
+            config = {}
+            YAML.load(ERB.new(File.read(config_path(file_name))).result).each do |k, v|
+              config["#{namespace}_#{k}"] = v
+            end
+            config
+          rescue
+            raise "File #{file_name} does not exist in config" unless File.exists?(config_path(file_name))
+            raise "Invalid config file #{file_name}"
+          end
         end
       end
-
     end
   end
 end
--- lib/multi_config/railtie.rb
+++ lib/multi_config/railtie.rb
@@ -4,8 +4,8 @@ module MultiConfig
   class Railtie < Rails::Railtie
     initializer "multi_config.active_record" do |app|
       if defined? ::ActiveRecord
-        require 'multi_config/active_record'
-        ActiveRecord::Base.send(:extend, MultiConfig::ORMs::ActiveRecord)
+        require 'multi_config/orms/active_record'
+        ActiveRecord::Base.send(:include, MultiConfig::ORMs::ActiveRecord)
       end
     end
   end
